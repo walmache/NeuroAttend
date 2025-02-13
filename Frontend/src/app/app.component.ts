f782d395516418cb87a6cb04d61d9e86fee52503
Wilfrido Almache <walmache@gmail.com>
2025-02-12 20:51:55 -0500

    instalacion adminlte3

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from './admin-lte/sidebar/sidebar.component';
+import { LayoutComponent } from './admin-lte/layout/layout.component';
+import { HeaderComponent } from './admin-lte/header/header.component';
+import { FooterComponent } from './admin-lte/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    SidebarComponent,
+    LayoutComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
